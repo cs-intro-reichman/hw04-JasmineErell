e40cee4983f533ff634dacc3138d0d43ce2ff2c7
JasmineErell <jasmine.erell@gmail.com>
2024-01-16 22:39:44 +0200

    fix 6

--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,171 @@
-public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
-    public static void main(String[] args) {
+public class StringOps 
+{
+    public static void main(String[] args) 
+    {
+        String str = "MMMM";
+        char a = 'M';
+        System.out.println(allIndexOf(str, a)); 
+    }
+
+    public static String capVowelsLowRest (String str) 
+    {
+        String res = "";
+        for (int i = 0; i<str.length(); i++)
+        {
+            str = lowerCase(str);
+            char ch = str.charAt(i);
+            int ascii = ch ;
+            if(str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'a' || str.charAt(i) == 'u' || str.charAt(i) == 'o')
+            {
+                int asciiLow = ascii - 32; 
+                res = res + ((char)asciiLow);
+            }
+            else
+            {
+                res += str.charAt(i);
+            }
+        }
+        return res;
+    }
+
+    public static String camelCase (String str) 
+    {
+       
+        String res = "";
+        String temp = "";
+        int start = 0;
+        int stop = 0;
+        int c = amountOfSpaces(str);
+        if(c == 0)// if there is no space, return the word in small leters
+        {
+            res = lowerCase(str);
+        }
+        else// there are some spaces
+        {
+           for(int i = 0; i<=c; i++)
+           {
+                if(start == 0)// makes the first word in small leters
+                {
+                    stop = str.indexOf(' ');
+                    res+= lowerCase(str.substring(start,stop));
+                    
+                }
+                else
+                {
+                    stop = str.indexOf(' ',start);
+                    if(stop == -1)
+                    {
+                        res+= makesItPerfect(str.substring(start));
+                    }
+                    else
+                    {
+                        res+= makesItPerfect(str.substring(start, stop));
+                        start = stop+1;
+                    }
+                }
+                start = stop+1;
+           }
+        }
+        return res ;
+    }
+
+    public static int[] allIndexOf (String str, char a) 
+    {
+        int arrayIndex = 0;
+        int c = 0;
+        for(int i = 0; i<str.length(); i++)
+        {
+            if(str.charAt(i)== a)
+            {
+                c++;
+            }
+        }
+        int [] array =new int [c];
+        if(c==0)
+        {
+            array[c] = 0;
+        }
+        else
+        {
+            for (int j=0; j<str.length(); j++)
+            {
+                if(str.charAt(j) == a)
+                {
+                    array[arrayIndex] = j;
+                    arrayIndex++;
+                }
+            }
+        }
+        return array;
         
     }
-
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static boolean isThereSpace(String str) 
+    {
+        boolean flag = false;
+        for(int i = 0; i<str.length(); i++)
+        {
+            if(str.charAt(i)!= ' ')
+            {
+                continue;
+            }
+            else
+            {
+                flag = true;
+                break;
+            }
+        }
+      
+        return flag;
     }
-
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String makesItPerfect(String str) 
+    {
+        String res = "";
+        char ch = str.charAt(0);
+        int ascii = ch ;
+        if (ascii>=97 && ascii<=122)// if small leter make it capital
+            {
+                int asciiC = ascii - 32; 
+                res = res + ((char)asciiC);
+            }
+        else
+        {
+            res += str.charAt(0);
+        }
+        for (int i = 1; i< str.length(); i++)
+        {
+            ch = str.charAt(i);
+            ascii = ch ;
+            if (ascii>=64 && ascii<=90)// if capital leter make it small
+            {
+                int asciiLow = ascii + 32; 
+                res = res + ((char)asciiLow);
+            }
+            else
+            {
+                res += str.charAt(i);
+            }
+        }
+      
+        return res;
     }
-
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static String lowerCase(String s) 
+    {
+        
+        String res = "";
+        for (int i = 0; i< s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            int ascii = ch ;
+            if (ascii>=65 && ascii<=94)
+            {
+                int asciiLow = ascii + 32; 
+                res = res + ((char)asciiLow);
+            }
+            else
+            {
+                res = res + ch;
+            }
+        }
+        return res;
     }
 }
